43ff515d633f87479103993c4575b072fe703467
Alvaro <alvaritodavilag0331@gmail.com>
2024-05-02 01:16:40 -0600

    Pestania con treeview vacia

--- GUI/empleados.py
+++ GUI/empleados.py
@@ -3,3 +3,25 @@ from tkinter import END, messagebox, ttk
 
 def crear_contenido(tab):
     label=tk.Label(tab, text="Pestaña empleados").pack(padx=10, pady=10)
+    
+    #boton para ver los empleados registrados
+    boton=ttk.Button(tab, text="Lista de empleados", command=empleados_lista).pack(pady=10)
+    
+def empleados_lista():
+    #mostrar una nueva ventana
+    ventana_empleados=tk.Toplevel()
+    ventana_empleados.title("Lista de empleados")
+    
+    #crear treeview
+    tree = ttk.Treeview(ventana_empleados)
+    tree.pack(expand=True, fill=tk.BOTH)
+    #configurar columnas
+    tree["columns"]=("ID","Nombre","Telefono","Turno")
+    tree.column("#0", width=0,stretch=tk.NO) #ocultar primera columna
+    
+    #configurar encabezados
+    tree.heading("ID",text="ID")
+    tree.heading("Nombre",text="Nombre")
+    tree.heading("Telefono",text="Telefono")
+    tree.heading("Turno",text="Turno")
+    
