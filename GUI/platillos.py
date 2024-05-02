3ecce8d0505d6c1a96ca5b3cb73bc13749b79a27
Alvaro <alvaritodavilag0331@gmail.com>
2024-05-01 23:43:01 -0600

    agregadas mas pestanias al notebook

---
+++ GUI/platillos.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+from tkinter import END, messagebox, ttk
+
+def crear_contenido(tab):
+    label=tk.Label(tab, text="Pestaña platillos").pack(padx=10, pady=10)
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import END, messagebox, ttk
 from GUI.empleados import crear_contenido as empleados
+from GUI.proveedor import crear_contenido as proveedores
+from GUI.platillos import crear_contenido as platillos
 
 def run_app():
     root=tk.Tk()
@@ -11,13 +13,21 @@ def run_app():
     
     #Pestañas del notebook
     tab1=ttk.Frame(notebook)
+    tab2=ttk.Frame(notebook)
+    tab3=ttk.Frame(notebook)
     
     
     #Agregar contenido a las pestañas del notebook
     empleados(tab1)
+    proveedores(tab2)
+    platillos(tab3)
+    
+    
     
     #agregar pestañas al notebook
     notebook.add(tab1, text="Empleados")
+    notebook.add(tab2, text="Proveedores")
+    notebook.add(tab3, text="Platillos")
     
     
     #Colocar notebook en la pestaña principal
