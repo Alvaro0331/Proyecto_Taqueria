d068a887bc97717dc628778ec30192500a378328
Alvaro <alvaritodavilag0331@gmail.com>
2024-05-02 23:11:11 -0600

    Creada funcion para actualizar empleados

--- DB/dbempleado.py
+++ DB/dbempleado.py
@@ -1,3 +1,4 @@
+from tkinter import messagebox
 from DB.dbmanager import dbManager
 
 def obtener_meseros():
@@ -15,3 +16,20 @@ def obtener_meseros():
     db.close()
     #Retornar resultados
     return resultados
+
+def actualizar_mesero(id_empleado, nombre, telefono, turno):
+    exito=False
+    db=dbManager()
+    #Consulta SQL
+    try:
+        query="UPDATE empleados SET nombre=%s, telefono=%s, turno=%s WHERE id=%s"
+        values = (nombre, telefono, turno, id_empleado)
+        db.cursor.execute(query)
+        #guardar cambios en la base de datos
+        db.conn.commit()
+        exito=True
+    except Exception as error:
+        messagebox.showerror("Error al actualizar los datos", error)
+    
+    db.close()
+    return exito
