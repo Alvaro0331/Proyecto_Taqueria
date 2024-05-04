041059330dcfb96d6eeca91532503c3514435da2
Alvaro <alvaritodavilag0331@gmail.com>
2024-05-04 15:51:13 -0600

    Contenido de la interfaz acomodado pero vacio

--- GUI/platillos.py
+++ GUI/platillos.py
@@ -2,4 +2,33 @@ import tkinter as tk
 from tkinter import END, messagebox, ttk
 
 def crear_contenido(tab):
-    label=tk.Label(tab, text="Pestaña platillos").pack(padx=10, pady=10)
+    # Crear un Frame para contener el Treeview y el Scrollbar
+    frame = tk.Frame(tab)
+    frame.pack(fill=tk.BOTH, expand=True)
+    
+    # Crear el Treeview
+    tree = ttk.Treeview(frame)
+    tree.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    
+    # Crear un Scrollbar vertical
+    scrollbar = ttk.Scrollbar(frame, orient="vertical", command=tree.yview)
+    scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+    
+    # Configurar el Treeview para que use el Scrollbar vertical
+    tree.configure(yscrollcommand=scrollbar.set)
+    
+    #configurar columnas
+    tree["columns"]=("ID","Nombre","Comentarios")
+    tree.column("#0", width=0,stretch=tk.NO) #ocultar primera columna
+    
+    #configurar encabezados
+    tree.heading("ID",text="ID")
+    tree.heading("Nombre",text="Nombre")
+    tree.heading("Comentarios",text="Comentarios")
+    
+    # Crear un Frame para contener botones
+    frame = tk.Frame(tab)
+    frame.pack(fill=tk.BOTH, expand=True)
+    #Boton para registrar nuevo platillo
+    botonPlatoAlta=ttk.Button(tab,text="Registrar platillo")#, command=lambda: validar_campos(nombreEntry, telefonoEntry, turnoCombobox))
+    botonPlatoAlta.place(x=100,y=420)
