7e44c04c9bb07be702470a6929fbc11416ae2c50
Alvaro <alvaritodavilag0331@gmail.com>
2024-05-15 20:39:10 -0600

    Muestro detalle de facturas y guardo elementos

--- DB/dbfacturas.py
+++ DB/dbfacturas.py
@@ -21,3 +21,87 @@ def obtener_facturas():
     db.close()
     # Retornar resultados
     return resultados
+
+def obtener_detalle_factura(id_factura):
+    #Instancia del dbManager
+    db=dbManager()
+    
+    #Consulta SQL
+    query="SELECT ID_DetalleF, Cantidad, FK_Producto, FK_Ingredientes FROM detalle_factura WHERE FK_Factura_Compra = %s"
+    values=(id_factura,)
+    db.cursor.execute(query,values)
+    
+    #Obtener resultados de la busqueda
+    resultados=db.cursor.fetchall()
+    
+    #Cerrar conexion
+    db.close()
+    #Retornar resultados
+    return resultados
+
+def lista_productos():
+    #Instancia del dbManager
+    db=dbManager()
+    
+    #Consulta SQL
+    query="SELECT ID_Producto, Nombre FROM producto"
+    db.cursor.execute(query)
+    
+    #Obtener resultados de la busqueda
+    productos=db.cursor.fetchall()
+    
+    # Agregar tipo de elemento ("Producto") a cada tupla
+    productos_con_tipo = [(id_producto, nombre, "Producto") for id_producto, nombre in productos]
+    
+    #Cerrar conexion
+    db.close()
+    #Retornar resultados
+    return productos_con_tipo
+
+def lista_ingredientes():
+    #Instancia del dbManager
+    db=dbManager()
+    
+    #Consulta SQL
+    query="SELECT ID_Ingredientes, Nombre FROM ingredientes"
+    db.cursor.execute(query)
+    
+    #Obtener resultados de la busqueda
+    ingredientes=db.cursor.fetchall()
+    
+    # Agregar tipo de elemento ("Ingrediente") a cada tupla
+    ingredientes_con_tipo = [(id_ingrediente, nombre, "Ingredientes") for id_ingrediente, nombre in ingredientes]
+    
+    #Cerrar conexion
+    db.close()
+    #Retornar resultados
+    return ingredientes_con_tipo
+
+def agregar_detalle_factura(id_elemento,tipo,cantidad,id_factura):
+    exito = False
+    db = dbManager()
+    try:
+        # Obtener el nombre de la columna de la llave foránea (FK_Producto o FK_Ingredientes)
+        columna_fk = f"FK_{tipo}"
+        
+        # Consulta SQL para obtener el ID del elemento seleccionado (producto o ingrediente)
+        query_id = f"SELECT ID_{tipo} FROM {tipo.lower()} WHERE ID_{tipo} = %s"
+        db.cursor.execute(query_id, (id_elemento,))
+        id_elemento_existente = db.cursor.fetchone()
+        
+        if id_elemento_existente:
+            # Consulta SQL para insertar en la tabla detalle_factura
+                query_insert = f"INSERT INTO detalle_factura (Cantidad,FK_Factura_Compra, Tipo, {columna_fk}) VALUES (%s, %s, %s, %s)"
+                values_insert = (cantidad,id_factura,tipo,id_elemento)
+                db.cursor.execute(query_insert, values_insert)
+                # Guardar cambios
+                db.conn.commit()
+                exito = True
+        else:
+            messagebox.showerror("Error", f"No se encontró el {tipo} con el ID {id_elemento}.")
+    except Exception as error:
+        messagebox.showerror("Error al agregar detalle de comanda", error)
+    finally:
+        # Cerrar conexión
+        db.close()
+    return exito
--- GUI/facturas.py
+++ GUI/facturas.py
@@ -29,8 +29,121 @@ def crear_contenido(tab):
         treeview.insert("", tk.END, values=resultado)
     
     #Agregar el evento select al treeview
-    treeview.bind("<<TreeviewSelect>>")#, lambda event: detalles_facturas(treeview))
+    treeview.bind("<<TreeviewSelect>>", lambda event: detalles_facturas(treeview))
     
     # Crear un Frame para contener formulario
     frame = tk.Frame(tab)
     frame.pack(fill=tk.BOTH, expand=True)
+    
+
+#Ventana para detalle de factura#
+def detalles_facturas(treeview):
+    # Obtener el registro seleccionado
+    item = treeview.focus()
+
+    # Obtener los detalles del registro seleccionado
+    detalles = treeview.item(item, "values")
+    
+    if detalles:
+        id_factura = detalles[0]  # Primer valor es el ID del empleado
+        print("ID de la factura seleccionada:", id_factura)  # Imprimir el ID en la consola
+        # Crear y configurar la ventana emergente
+        ventana_emergente = tk.Toplevel()
+        ventana_emergente.title("Detalles de factura")
+        
+        # Crear frame para el Treeview
+        frame_treeview = tk.Frame(ventana_emergente)
+        frame_treeview.pack(pady=10, fill="both", expand=True)
+
+        # Crear el Treeview para mostrar los detalles de la comanda
+        treeview_detalles = ttk.Treeview(ventana_emergente, columns=("ID_DetalleF", "Cantidad","FK_Producto", "FK_Ingrediente"), show="headings")
+        treeview_detalles.heading("ID_DetalleF", text="ID Detalle")
+        treeview_detalles.heading("Cantidad", text="Cantidad")
+        treeview_detalles.heading("FK_Producto", text="FK Producto")
+        treeview_detalles.heading("FK_Ingrediente", text="FK Ingrediente")
+        
+        # Agregar scrollbar vertical
+        scrollbar_y = ttk.Scrollbar(ventana_emergente, orient="vertical", command=treeview_detalles.yview)
+        scrollbar_y.pack(side="right", fill="y")
+        treeview_detalles.configure(yscrollcommand=scrollbar_y.set)
+        
+        #Obtener los resultados de la consulta SQL
+        resultados = obtener_detalle_factura(id_factura)
+
+        # Agregar los resultados al treeview
+        for resultado in resultados:
+            treeview_detalles.insert("", tk.END, values=resultado)
+        
+        treeview_detalles.pack(fill=tk.BOTH, expand=True)
+        
+        ##Formulario para agregar productos al detalle##
+        # Crear frame para el formulario de agregar productos
+        frame_formulario = tk.Frame(ventana_emergente)
+        frame_formulario.pack(pady=10)
+        
+        #Obtener ingredientes y productos disponibles
+        ingredientes=lista_ingredientes()
+        productos=lista_productos()
+
+        # Campos del formulario
+        lbl_producto = tk.Label(frame_formulario, text="Producto/Ingrediente:")
+        lbl_producto.grid(row=0, column=0, padx=5)
+        combo_producto = ttk.Combobox(frame_formulario, values=[f"{id_producto}: {nombre} ({tipo})" for id_producto, nombre, tipo in ingredientes + productos], state="readonly")
+        combo_producto.grid(row=0, column=1, padx=5)
+        combo_producto.config(values=[f"{id_producto}: {nombre} ({tipo})" for id_producto, nombre, tipo in ingredientes + productos],)
+        lbl_cantidad = tk.Label(frame_formulario, text="Cantidad:")
+        lbl_cantidad.grid(row=1, column=0, padx=5)        
+        entry_cantidad = tk.Entry(frame_formulario)
+        entry_cantidad.grid(row=1, column=1, padx=5)
+        #Boton
+        btn_agregar=tk.Button(frame_formulario,text="Agregar", command=lambda: validar_campos(combo_producto,entry_cantidad,id_factura, treeview,ventana_emergente))
+        btn_agregar.grid(row=3, column=1, padx=5)
+
+        # Hacer que la ventana emergente sea modal
+        ventana_emergente.grab_set()
+        ventana_emergente.focus_set()
+        
+        # Esperar hasta que la ventana se cierre y luego actualizar los datos de la comanda
+        ventana_emergente.wait_window()
+        
+        # Actualizar los datos de la comanda en el treeview
+        treeview.delete(*treeview.get_children())
+        resultados = obtener_facturas()
+        for resultado in resultados:
+            treeview.insert("", tk.END, values=resultado)
+
+
+##Validar campos##
+def validar_campos(combo_producto,entry_cantidad,id_factura, treeview,ventana_emergente):
+    elementoSeleccionado=combo_producto.get()
+    cantidad=entry_cantidad.get()
+    
+    if not elementoSeleccionado or not cantidad:
+        messagebox.showerror("Error", "Faltan campos por llenar.")
+    else:
+        id_elemento, tipo_elemento = obtener_id_tipo_elemento(elementoSeleccionado)
+        if id_elemento is not None and tipo_elemento is not None:
+            exito=agregar_detalle_factura(id_elemento,tipo_elemento,cantidad,id_factura)
+            
+            # Verificar si la inserción fue exitosa
+            if exito:
+                # Actualizar el treeview con los nuevos datos
+                treeview.delete(*treeview.get_children())
+                resultados = obtener_detalle_factura(id_factura)
+                for resultado in resultados:
+                    treeview.insert("", tk.END, values=resultado)
+                # Cerrar la ventana emergente
+                ventana_emergente.destroy()
+        else:
+            messagebox.showerror("Error", "No se pudo obtener el ID y el tipo del elemento seleccionado.")
+
+# Función para obtener el ID y el tipo del elemento seleccionado en el Combobox
+def obtener_id_tipo_elemento(elementoSeleccionado):
+    # Split para obtener el nombre y el tipo del elemento
+    nombre_elemento, tipo_elemento = elementoSeleccionado.rsplit(" (", 1)
+
+    # Split para obtener solo el ID del elemento
+    id_elemento = nombre_elemento.split()[0]
+
+    # Retornar el ID y el tipo del elemento
+    return id_elemento, tipo_elemento[:-1]  # Eliminar el paréntesis del tipo
